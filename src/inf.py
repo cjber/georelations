075ebe1231bc59930fc3c52ee99ccf6693566d05
cjber <cjberragan@gmail.com>
2021-09-24 16:59:31 +0100

    use new entity labels

--- src/inf.py
+++ src/inf.py
@@ -1,16 +1,14 @@
 import hydra
-from omegaconf.dictconfig import DictConfig, ValueNode
 import pytorch_lightning as pl
 import torch
-from torch.utils.data.dataloader import DataLoader
-from tqdm import tqdm
-
-from src.common.utils import load_envs
-from src.common.utils import PROJECT_ROOT
+from omegaconf.dictconfig import DictConfig, ValueNode
+from src.common.utils import PROJECT_ROOT, load_envs
 from src.pl_data.text_dataset import TextDataset
 from src.pl_modules.ensemble import RelationEnsemble
 from src.pl_modules.ger_model import GERModel
 from src.pl_modules.rbert_model import RBERT
+from torch.utils.data.dataloader import DataLoader
+from tqdm import tqdm
 
 pl.seed_everything(42)
 torch.use_deterministic_algorithms(True)
@@ -30,9 +28,8 @@ def load_dataset(datadir: ValueNode, coref_model: str):
     return DataLoader(dataset=ds, batch_size=64, shuffle=True)
 
 
-def run(cfg: DictConfig):
-    ger_model = load_pretrained_model(GERModel, cfg.model.ger_model, "cuda")
-    rel_model = load_pretrained_model(RBERT, cfg.model.rel_model, "cuda")
+def run():
+    rel_model = load_pretrained_model(RBERT, "", "cuda")
 
     model = RelationEnsemble(ger_model, rel_model)
     model = model.to("cuda")
@@ -50,18 +47,13 @@ def run(cfg: DictConfig):
         text = pred[0][0]
         rel = pred[0][1]
 
-        e1 = text.split("<e1>")[1].split("</e1>")[0].strip()
-        e2 = text.split("<e2>")[1].split("</e2>")[0].strip()
+        e1 = text.split("<head>")[1].split("</head>")[0].strip()
+        e2 = text.split("<tail>")[1].split("</tail>")[0].strip()
         triple = (e1, e2, rel)
         triples.append(triple)
 
     breakpoint()
 
 
-@hydra.main(config_path=str(PROJECT_ROOT / "conf"), config_name="ens")
-def main(cfg: DictConfig):
-    run(cfg)
-
-
 if __name__ == "__main__":
-    main()
+    run()
